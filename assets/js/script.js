ed376e29f1f6ff758b0f6c30c69a46202f1db786
Vijay Choudhary <scriptyvijay@gmail.com>
2022-06-09 19:53:36 +0530

    code optimized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,58 @@
-window.onload = function () {
-	let seconds = 0;
-	let tens = 0;
-	let appendTens = document.getElementById("tens");
-	let appendSeconds = document.getElementById("seconds");
-	let buttonStart = document.getElementById("button-start");
-	let buttonStop = document.getElementById("button-stop");
-	let buttonReset = document.getElementById("button-reset");
-	let Interval;
+// All HTML components Added First
 
-	buttonStart.onclick = function () {
-		clearInterval(Interval);
-		Interval = setInterval(startTimer, 10);
-	};
+let seconds = 0;
+let tens = 0;
+let appendTens = document.getElementById("tens");
+let appendSeconds = document.getElementById("seconds");
+let buttonStart = document.getElementById("button-start");
+let buttonStop = document.getElementById("button-stop");
+let buttonReset = document.getElementById("button-reset");
+let Interval;
 
-	buttonStop.onclick = function () {
-		clearInterval(Interval);
-	};
+// Start Button On Click Event Handling
 
-	buttonReset.onclick = function () {
-		clearInterval(Interval);
-		tens = "00";
-		seconds = "00";
-		appendTens.innerHTML = tens;
-		appendSeconds.innerHTML = seconds;
-	};
-
-	function startTimer() {
-		tens++;
-
-		if (tens <= 9) {
-			appendTens.innerHTML = "0" + tens;
-		}
-
-		if (tens > 9) {
-			appendTens.innerHTML = tens;
-		}
-
-		if (tens > 99) {
-			seconds++;
-			appendSeconds.innerHTML = "0" + seconds;
-			tens = 0;
-			appendTens.innerHTML = "0" + 0;
-		}
-
-		if (seconds > 9) {
-			appendSeconds.innerHTML = seconds;
-		}
-	}
+buttonStart.onclick = function () {
+	clearInterval(Interval);
+	// In every 10 Milliseconds setInterval Will Run startTimer Function
+	Interval = setInterval(startTimer, 10);
 };
+
+// Stop Button On Click Event Handling
+
+buttonStop.onclick = function () {
+	clearInterval(Interval);
+};
+
+// Reset Button On Click Event Handling
+
+buttonReset.onclick = function () {
+	clearInterval(Interval);
+	tens = "00";
+	seconds = "00";
+	appendTens.innerHTML = tens;
+	appendSeconds.innerHTML = seconds;
+};
+
+// Start Timer Function
+
+function startTimer() {
+	tens++;
+	//In every 10 Milliseconds tens will add 1;
+	if (tens <= 9) {
+		appendTens.innerHTML = "0" + tens;
+	}
+
+	if (tens > 9) {
+		appendTens.innerHTML = tens;
+	}
+
+	if (tens > 99) {
+		seconds++;
+		appendSeconds.innerHTML = "0" + seconds;
+		tens = 0;
+		appendTens.innerHTML = "0" + 0;
+	}
+	if (seconds > 9) {
+		appendSeconds.innerHTML = seconds;
+	}
+}
